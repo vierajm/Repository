eb1f22dce041badbf6f09f9f3e81d3b84cf26b08
vierajm <vierajm@my.palmbeachstate.edu>
2024-06-09 13:24:04 -0400

    Create Module5Lambda.cs

---
+++ Module5Lambda.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda {
+	class Program {
+		static void Main(string[] args) {
+
+			var myNumbers = new List<int> { 0, 0, 2, 3, 4, 4, 5, 9, 11, 14, 14, 18 };
+
+			var numbersOver9 = myNumbers.Where(number => number > 9);
+
+			foreach (var number in numbersOver9) {
+				Console.WriteLine(number);
+			}
+
+		}
+	}
+}
