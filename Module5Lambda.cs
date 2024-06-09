a204ecef7555261fbda45a98e58cc964ab47aa00
vierajm <vierajm@my.palmbeachstate.edu>
2024-06-09 13:47:44 -0400

    Update Module5Lambda.cs

--- Module5Lambda.cs
+++ Module5Lambda.cs
@@ -8,10 +8,14 @@ namespace Lambda {
 
 			var myNumbers = new List<int> { 0, 0, 2, 3, 4, 4, 5, 9, 11, 14, 14, 18 };
 
-			var numbersOver9 = myNumbers.Where(number => number > 9);
+			 // var numbersOver9 = myNumbers.Where(number => number > 9);
 
-			foreach (var number in numbersOver9) {
-				Console.WriteLine(number);
+			 // foreach (var number in numbersOver9) {
+			 // Console.WriteLine(number);
+			var OddNumbers = myNumbers.Where(number => number % 2 == 1);
+
+			foreach (var number in OddNumbers) {
+			Console.WriteLine(number);
 			}
 
 		}
