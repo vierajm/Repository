a6826dc8974758deb009d821c2064422d3a29913
Juliette Viera <vierajm@my.palmbeachstate.edu>
2024-06-15 11:52:40 -0400

    615 Commit

--- Module5Discussion/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Module5Discussion/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a93a2b2908728a79dcf592345fabf1f9b98614d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2052bf4b5df0b64867af14db4fa2932300ecb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
