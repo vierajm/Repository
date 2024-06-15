eccb0f2cadb604ffbac49bf78a1c9cef857acdf1
Juliette Viera <vierajm@my.palmbeachstate.edu>
2024-06-15 13:24:58 -0400

    124 commit

---
+++ Module7Discussion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine("What is your favorite fruit?");
+string favoritefruit = Console.ReadLine();
+
+switch (favoritefruit)
+{
+    case "Bananas":
+        Console.WriteLine("Nice! Bananas are high in potassium.");
+        break;
+    case "Apples":
+        Console.WriteLine("Great! Apples contain a lot of fiber.");
+        break;
+    case "Grapes":
+        Console.WriteLine("Good! Grapes are good for bone health.");
+        break;
+    default:
+        Console.WriteLine("That's great! Do you know what health benefits your favorite fruit provides?");
+        break;
+
+}
