ed55041ace4288a897c10973fe74c2aec9ac4bfc
Juliette Viera <vierajm@my.palmbeachstate.edu>
2024-06-15 13:48:10 -0400

    148 commit

--- Module7Discussion/obj/Debug/net8.0/Module7Discussion.AssemblyInfo.cs
+++ Module7Discussion/obj/Debug/net8.0/Module7Discussion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module7Discussion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d694015719ab7963dd3986858a97ae2ab6215e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccb0f2cadb604ffbac49bf78a1c9cef857acdf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module7Discussion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module7Discussion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
